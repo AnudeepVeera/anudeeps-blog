2350b8c623061636d701729da4c3aa586204fcc1
AnudeepVeera <anudeep.veera96@gmail.com>
2023-03-28 13:39:48 +0530

    required component creation was done

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'anudeeps-blog-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
